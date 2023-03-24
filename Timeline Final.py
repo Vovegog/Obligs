cc79aedb756245fcb79f2a7a8f6db095512771bc
Vovegog <126813872+Vovegog@users.noreply.github.com>
2023-03-24 02:48:02 +0100

    Now?

--- Timeline Final.py
+++ Timeline Final.py
@@ -16,9 +16,8 @@ def read_cards(file_path):
     #     card = temp_list[i]
     #     card = card.split(",")
     #     game_list.append(card)
-    temp_list = cards.split("\n")
-    temp_list.split(",")
-    return temp_list
+    game_list = cards.split("\n").split(",")
+    return game_list
 
 def clear_screen():
     """
